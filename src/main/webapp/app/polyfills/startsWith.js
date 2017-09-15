c0ca5e68162ace5c2f4f9bb9f2e52dbf5a38485b
Wouter Samaey <wouter.samaey@storefront.be>
2017-09-15 14:16:19 +0200

    Added a JS polyfill for IE for “startsWith” function

---
+++ src/main/webapp/app/polyfills/startsWith.js
@@ -0,0 +1,9 @@
+if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function(searchString, position) {
+        position = position || 0;
+        return this.indexOf(searchString, position) === position;
+    };
+}
+
+
+console.log('POLYFILLLS LOADED!');
