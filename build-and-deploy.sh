b10d6ac7713f6117c8f9dfa9ac7506c683176625
Wouter Samaey <wouter.samaey@storefront.be>
2017-09-15 14:16:36 +0200

    Improved build and deploy scripts

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 echo "Building..."
 ./mvnw -Pprod package -DskipTests
 
@@ -10,7 +12,12 @@ echo "Waiting..."
 sleep 3
 
 echo "Transfering new files"
-./upload-to-production.sh
+rsync -rltz --progress --delete \
+--perms \
+--chmod=u=rwX,g=rwX,o=rX \
+--rsh "ssh" \
+"$DIR"/target/*.war root@87.238.165.25:/var/fsprocloud/
+
 
 echo "Starting service"
 ssh root@87.238.165.25 '/etc/init.d/fsprocloud start'
